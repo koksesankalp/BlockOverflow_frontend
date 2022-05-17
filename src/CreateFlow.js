df6fc78e6e5f4fc87b20af2bcaf4619a84a7094a
Aditya <aditya26sg@gmail.com>
2022-05-18 02:33:29 +0530

    added accordion to view answers

--- src/CreateFlow.js
+++ src/CreateFlow.js
@@ -527,6 +527,11 @@ export const CreateFlow = () => {
   var modal = document.getElementById("myModal");
   var modal2 = document.getElementById("myModal2");
 
+  async function openAccordion(quesId) {
+    setCurrentDoubtAnsweringId(quesId);
+    await getAnswer(quesId);
+  }
+
   // open the modal 1
   async function openModal(quesId) {
     setCurrentDoubtAnsweringId(quesId);
@@ -671,57 +676,37 @@ export const CreateFlow = () => {
                 <button id="modalButton" onClick={() => openModal(doubt.quesId)}>Show Answers</button>
                 <button id="modalButton2" onClick={() => openModal2(doubt.quesId)}>Answer Question</button>
               </div>
+
+              <div class="accordion accordion-flush" id="accordionFlushExample">
+                <div class="accordion-item" onClick={() => openAccordion(doubt.quesId)}>
+                  <h2 class="accordion-header" id="flush-headingOne">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                      View Answers
+                    </button>
+                  </h2>
+                  <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body">
+                      {allAnswers.map((answer, index) => {
+                        return (
+                          <div key={index}>
+                            <span className="totalUpvotes">{answer.upvotes}</span>&nbsp;
+                            <button onClick={() => { upvoteCurrentAnswer(answer.ansId) }} className="upvoteArrow">&#8679;</button>
+                            &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+                            <span className="answerBody">Answer {answer.ansId}:- {answer.answerbody}</span>
+                            <hr></hr>
+                            <br></br>
+                          </div>
+                        )
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           )
         })}
 
         {/* Modal 1 for displaying answers; convert it to accordian */}
-
-        <div class="accordion accordion-flush" id="accordionFlushExample">
-          <div class="accordion-item">
-            <h2 class="accordion-header" id="flush-headingOne">
-              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                Accordion Item #1
-              </button>
-            </h2>
-            <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-              <div class="accordion-body">
-                {allAnswers.map((answer, index) => {
-                  return (
-                    <div key={index}>
-                      <span className="totalUpvotes">{answer.upvotes}</span>&nbsp;
-                      <button onClick={() => { upvoteCurrentAnswer(answer.ansId) }} className="upvoteArrow">&#8679;</button>
-                      &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-                      <span className="answerBody">Answer {answer.ansId}:- {answer.answerbody}</span>
-                      <hr></hr>
-                      <br></br>
-                    </div>
-                  )
-                })}
-              </div>
-            </div>
-          </div>
-          <div class="accordion-item">
-            <h2 class="accordion-header" id="flush-headingTwo">
-              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                Accordion Item #2
-              </button>
-            </h2>
-            <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-              <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
-            </div>
-          </div>
-          <div class="accordion-item">
-            <h2 class="accordion-header" id="flush-headingThree">
-              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                Accordion Item #3
-              </button>
-            </h2>
-            <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-              <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
-            </div>
-          </div>
-        </div>
 
         <div id="myModal" className="modal">
           <div className="modal-content">
